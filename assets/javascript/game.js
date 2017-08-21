38ae00646902bb98216988a101c7816131aab5eb
Denny Wu <denny.wu87@gmail.com>
2017-08-20 23:29:35 -0700

    Working on fixing errors in javascript code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,41 +18,42 @@ var currentAnswers = document.getElementById("playerInput");
 
 var gameState = {
 
-	wins = 0,
-	losses = 0,
-	guessLeft = 9,
+	wins: 0,
+	losses: 0,
+	guessLeft: 9,
 
 	winGame: function() {
 
 		this.wins = this.wins + 1
 		playerWins.textContent = "Wins: " + this.wins
-	}
+	},
 
 	loseGame: function() {
 
 		this.losses = this.losses + 1
 		playerLoss.textContent = "Losses: " + this.losses
-	}
+	},
 
 	missGuess: function() {
 
 		this.guessLeft = this.guessLeft - 1
 		guessesLeft.textContent = "Guesses Left: " + this.guessLeft
-	}
+	},
 
 	outOfGuess: function() {
 
 		this.guessLeft = 9
 	}
 
-}
+};
 
-// The following is the code that governs gameplay.
+// The following is the code that governs gameplay. I suspect there is a problem with scope. The push function and outOfGuess don't seem to be working correctly.
 
 document.onkeyup = function(event) {
 
 	var userGuess = event.key;
 	var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+	//console.log(computerGuess);
 
 	if (userGuess === computerGuess) {
 		gameState.winGame()
